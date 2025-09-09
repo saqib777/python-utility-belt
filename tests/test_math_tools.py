771eba977f71d7d6c2fbae538601f34effe7279f
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2025-09-09 17:09:46 +0530

    Update test_math_tools.py

--- tests/test_math_tools.py
+++ tests/test_math_tools.py
@@ -15,5 +15,14 @@ class TestMathTools(unittest.TestCase):
     def test_is_prime_false(self):
         self.assertFalse(is_prime(8))
 
+    def test_gcd(self):
+        from utils.math_tools import gcd
+        self.assertEqual(gcd(12, 8), 4)
+
+    def test_lcm(self):
+        from utils.math_tools import lcm
+        self.assertEqual(lcm(12, 8), 24)
+
+
 if __name__ == "__main__":
     unittest.main()
